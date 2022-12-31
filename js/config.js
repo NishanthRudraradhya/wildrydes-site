1cd70f1e1971ab5ee3b81a7d46c45084b5c4eecb
R Nishanth <r.nishanth@powerschool.com>
2023-01-01 02:13:04 +0530

    new files

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_FqHpU6zp6', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '61ltok8gdprtn61i8qhsarq9q4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://atr0b0jeah.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
